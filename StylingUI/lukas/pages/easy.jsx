0e605b807998a75c6971403584475444d24d3e8c
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-07-22 23:52:33 -0400

    for practicing tailwind with lukas

---
+++ StylingUI/lukas/pages/easy.jsx
@@ -0,0 +1,13 @@
+export default function Easy() {
+  return (
+    <section className='grid gap-8 md:grid-cols-2 md:items-center md:text-left'>
+      <div>
+        <img className="w-full rounded-lg" src="https://assets.ofs.com/2020-03/OFS_DigitalWallpaper_Desktop_1080x1920_Calm.jpg" alt="calm_image" />
+      </div>
+      <div>
+        <h1 className="text-4xl font-medium">Headline</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat tempore ex, natus id beatae provident autem rerum officia, aliquid, adipisci nesciunt fuga cupiditate voluptatibus eius molestiae soluta recusandae voluptatum doloremque! Nihil mollitia id distinctio magni numquam dolore exercitationem error facilis consectetur corporis odit, reprehenderit voluptatem autem maxime. Blanditiis, incidunt! Necessitatibus, tempore beatae consectetur deleniti obcaecati dolorum sit, aperiam maiores, molestiae ipsa nisi corrupti distinctio illo rem! Eveniet reprehenderit sequi necessitatibus distinctio incidunt nisi non vel saepe tempora unde, mollitia animi exercitationem porro, optio nemo deleniti cumque id dolor explicabo deserunt! Corporis non aliquam magni atque distinctio nisi, amet minima consectetur.</p>
+      </div>
+    </section>
+  )
+}
