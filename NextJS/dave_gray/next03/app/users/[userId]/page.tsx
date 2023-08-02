e61870c6f7c099c2b05b1223fec10e4f4fd0fbaf
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-08-02 17:51:57 -0400

    dynamic routes & metadata

--- NextJS/dave_gray/next03/app/users/[userId]/page.tsx
+++ NextJS/dave_gray/next03/app/users/[userId]/page.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import getUser from '@/lib/getUser'
+import getUserPost from '@/lib/getUserPost'
+import {Suspense} from "react"
+import UserPosts from './components/UserPosts'
+import { Metadata } from 'next'
 
 type Params = {
   params: {
@@ -6,8 +10,33 @@ type Params = {
   }
 }
 
-export default function UserPage() {
+export async function generateMetadata({params: {userId}}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId)
+  const user: User = await userData
+
+  return {
+    title: user.name,
+    description: 'This is the page of ${user.name}'
+  }
+}
+
+
+export default async function UserPage({params: {userId}}: Params) {
+
+  const userData: Promise<User> = getUser(userId)
+  const userPostsData: Promise<Post[]> = getUserPost(userId)
+  // const [user, userPosts] = await Promise.all([userData, userPostsData])
+
+  const user = await userData
+  
   return (
-    <div>page</div>
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h3>Loading ...</h3>}>
+        {/* <UserPosts posts={userPosts} /> */}
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
   )
 }
--- NextJS/dave_gray/next03/app/users/page.tsx
+++ NextJS/dave_gray/next03/app/users/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import getAllUsers from "@/lib/getAllUsers";
 import Link from "next/link";
+import { log } from "console";
 
 export const metadata: Metadata = {
   title: 'Users',
@@ -10,7 +11,10 @@ export default async function UsersPage() {
   const usersData: Promise<User[]> = getAllUsers()
 
   const users = await usersData
- 
+
+  console.log("Hello");
+  
+
   const content = (
     <section>
       <h2>
