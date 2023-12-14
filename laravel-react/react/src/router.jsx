e2c58cce78a1c3eae995383732b1ea6dfdeeaccd
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-12-13 21:06:41 -0300

    added seed data, table for displaying the users and the delete function

--- laravel-react/react/src/router.jsx
+++ laravel-react/react/src/router.jsx
@@ -6,6 +6,7 @@ import Users from './views/Users';
 import DefaultLayout from './components/DefaultLayout';
 import GuestLayout from './components/GuestLayout';
 import Dashboard from './views/Dashboard';
+import UserForm from './views/UserForm';
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,14 @@ const router = createBrowserRouter([
       {
         path: '/Users',
         element: <Users />
+      },
+      {
+        path: '/Users/new',
+        element: <UserForm key="userCreate"/>
+      },
+      {
+        path: '/Users/:id',
+        element: <UserForm key="userUpdate"/>
       },
     ]
   },
---
+++ laravel-react/react/src/views/UserForm.jsx
@@ -0,0 +1,9 @@
+
+export default function Userform() {
+  
+  return(
+    <div>
+      User Form
+    </div>
+  )
+};
