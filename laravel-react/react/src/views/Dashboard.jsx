fdb7d18c5c9443d7d8df38d48f7601b5df2753f4
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-12-16 14:01:01 -0300

    finishing up, updated the dashboard, added screenshots, added more css

--- laravel-react/react/src/views/Dashboard.jsx
+++ laravel-react/react/src/views/Dashboard.jsx
@@ -1,6 +1,20 @@
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard-container">
+      <h2>Welcome <u>Admin</u> to Your Laravel-React Application</h2>
+      <p className="welcome-message">
+        This robust application combines the power of <strong>PHP Laravel</strong> on the backend with <strong>React</strong> on the frontend. Here's what you can do:
+      </p>
+      <ul className="dashboard-features">
+        <li><strong>User Management:</strong> Create, Read, Update, and Delete user accounts.</li>
+        <li><strong>Security:</strong> Secure login/logout functionality, ensuring data safety.</li>
+        <li><strong>Data Storage:</strong> Efficient use of a minimalistic SQLite database.</li>
+      </ul>
+      <p className="additional-info">
+        Your actions are backed by a secure, token-based authentication system, providing a seamless and secure user experience.
+      </p>
+      {/* Rest of your dashboard content */}
+    </div>
   )
 }
