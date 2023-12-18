939ee29fdf5a1882ff4206021c7d08e9764fce72
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-12-18 15:30:08 -0300

    Flat and Section List

---
+++ react-native/official_doc/FlatList.js
@@ -0,0 +1,45 @@
+/*
+!FlatList:
+* Displays a scrolling list of data.
+* Ideal for long lists where the number of items may change.
+* Renders only the elements currently visible on the screen, not all at once.
+* Requires data (source of information) and renderItem (formats and renders each item) props.
+*/
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+};
+
+export default FlatListBasics;
