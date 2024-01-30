56744b46363d71b456a404bc5ac9044861d896ed
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-01-30 12:58:18 -0300

    first app created

---
+++ react-native/codecademy/hello-world/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Text>My name is Nandor</Text>
+      <Text>This is my first React Native App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
