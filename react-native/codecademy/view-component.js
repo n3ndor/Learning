86d870bc03a7e37049f7a4f11cf8d65df39769bc
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-01-30 12:58:51 -0300

    practice with the view component

---
+++ react-native/codecademy/view-component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+const App = () =>
+<View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+  <View style={{width:100, height:100, backgroundColor:"red"}}></View>
+  <View style={{width:100, height:100, backgroundColor:"blue"}}></View>
+</View>
+  null;
+
+export default App;
