04b4f48e7423dbdcacf3f2809069d1cadfabf068
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-02-05 22:00:28 -0300

    stylesheet init

---
+++ react-native/codecademy/Exercising/stylesheets.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, backgroundColor: '#e5e5e5', justifyContent: 'center' }}>
+    <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+  </View>
+);
+
+export default App;
+
+// export const styles = StyleSheet.create({
+//  
+// });
