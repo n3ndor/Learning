4a554eafafe4daa9ff33c1a1dd0b5e51a28cce60
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-02-07 14:03:39 -0300

    solution for combining styles + pressable

--- react-native/codecademy/Exercising/combining-styles.js
+++ react-native/codecademy/Exercising/combining-styles.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {Pressable, StyleSheet, View } from 'react-native';
 
 const App = () => (
-  <View style={styles.layout}>
-    <Box />
+  <View style={styles.layout} >
+    <Pressable> {/* if you press the div will change the backgroundColor */}
+      {(state) => <Box pressed={state.pressed} />}
+    </Pressable>
   </View>
 );
 
 export default App;
 
-export const Box = () => (
-  <View style={styles.box} />
+export const Box = (props) => (
+    <View style={[styles.box, props.pressed && { backgroundColor: 'blue' }]} />
 );
 
 export const styles = StyleSheet.create({
