2ab59fb1913c55ce79e7adb0f3c3e9826107163f
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-02-07 13:50:59 -0300

    solution for stylesheets

--- react-native/codecademy/Exercising/stylesheets.js
+++ react-native/codecademy/Exercising/stylesheets.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const App = () => (
-  <View style={{ flex: 1, backgroundColor: '#e5e5e5', justifyContent: 'center' }}>
-    <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+  <View style={styles.layout}>
+    <View style={styles.card} />
+    <View style={styles.card} />
   </View>
 );
 
 export default App;
 
-// export const styles = StyleSheet.create({
-//  
-// });
+export const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: '#e5e5e5',
+    justifyContent: 'center',
+  },
+  card: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'white', 
+    margin: 16, 
+    borderRadius: 2, 
+    shadowColor: 'black', 
+    shadowOpacity: 0.3, 
+    shadowRadius: 1, 
+    shadowOffset: { height: 1, width: 0.3 } 
+  },
+});
