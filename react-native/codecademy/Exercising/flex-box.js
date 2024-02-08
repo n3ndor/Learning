22471f9edd3495179ee3a5b311c692a483b2792b
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-02-08 13:59:23 -0300

    solution flex box

--- react-native/codecademy/Exercising/flex-box.js
+++ react-native/codecademy/Exercising/flex-box.js
@@ -1,6 +1,6 @@
 //some information for flex: 1 or 2. 
 //example 1: 3x flex:1= the width is evenly for 3 divs
-//example 2: flex:1 + flex:2 + flex:1 = 
+//example 2: flex:1 + flex:2 + flex:1 = first and last div's take 1 spacec and the middle div takes double space
 
 
 import React, { useState } from 'react';
