8c6e5886842c7d6a5641b40b24b4010b6e0b6bb9
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-02-08 14:13:31 -0300

    init react navigation

---
+++ react-native/codecademy/Exercising/intro-react-navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// const Stack = createStackNavigator();
+
+const App = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>First screen</Text>
+  </View>
+);
+
+export default App;
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 16,
+  },
+});
