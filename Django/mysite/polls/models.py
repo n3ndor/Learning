c5c3bc2f90386df674a59eed1d2a514b0f11c803
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-03-22 14:41:27 -0300

    admin page updated, choises added, filter added, better displaying added, search added

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -13,6 +14,11 @@ class Question(models.Model):
         """Return a string representation of the question."""
         return self.question_text
     
+    @admin.display(
+    boolean=True,
+    ordering="pub_date",
+    description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
