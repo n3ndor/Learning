8326e66a5f947816806bd11d55b0922f898c35dd
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-07-27 13:56:03 -0400

    getting started with the next app

---
+++ Flutter/adv_basics_quiz_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:adv_basics_quiz_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
