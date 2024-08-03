b41067146565d5743ae4240339daac6cc0fa00a2
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-08-02 21:32:07 -0400

    added navigation and dummy data

--- Flutter/meals/test/widget_test.dart
+++ Flutter/meals/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/screens/categories.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
