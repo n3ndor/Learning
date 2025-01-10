5f313798e1696aaa25b554fae9673208677a5cea
Nandor <n3ndor@gmail.com>
2025-01-10 14:59:59 -0300

    function value, scope

--- Go/schwarzmueller/section2/investment_calculator/investment_calculator.go
+++ Go/schwarzmueller/section2/investment_calculator/investment_calculator.go
@@ -5,23 +5,41 @@ import (
 	"math"
 )
 
-func main(){
+const inflationRate = 2.5
 
-	const inflationRate = 2.5
+func main(){
 	var investmentAmount float64 
-	 expectedReturnRate := 5.5
+	expectedReturnRate := 5.5
 	var years float64
 
-	fmt.Print("Investment Amount: ")
+
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount  *  math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100 , years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// futureValue := investmentAmount  *  math.Pow(1 + expectedReturnRate / 100, years)
+	// futureRealValue := futureValue / math.Pow(1 + inflationRate / 100 , years)
+
+	// fmt.Println("Future Value: ", futureValue)
+
+	// fmt.Printf("Future Value: %v\n", futureValue)
+	// fmt.Printf("Future Value (adjusted for Inflation): %.1f\n", futureRealValue)
+
+	fmt.Printf(`Future Value: %.1f
+	Future Value (adjusted for Inflation): %.1f`, futureValue, futureRealValue)
+}
+
+func outputText(text string){
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount  *  math.Pow(1 + expectedReturnRate / 100, years)
+	rfv := fv / math.Pow(1 + inflationRate / 100 , years)
+	return fv, rfv
 }
