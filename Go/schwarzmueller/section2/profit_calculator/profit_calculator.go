dbd20bec5884fc533f281efee0124a6f5e5c1b0c
Nandor <n3ndor@gmail.com>
2025-01-13 13:49:14 -0300

    challenge

--- Go/schwarzmueller/section2/profit_calculator/profit_calculator.go
+++ Go/schwarzmueller/section2/profit_calculator/profit_calculator.go
@@ -1,46 +1,54 @@
 package main
 
-import "fmt"
-
-// func main(){
-// 	var revenue float64
-// 	var expenses float64
-// 	var taxRate float64
-
-// 	fmt.Print("Revenue: ")
-// 	fmt.Scan(&revenue)
-// 	fmt.Print("Expenses: ")
-// 	fmt.Scan(&expenses)
-// 	fmt.Print("Tax Rate: ")
-// 	fmt.Scan(&taxRate)
-
-// 	ebt := revenue - expenses
-// 	profit := ebt * (1 - taxRate / 100)
-// 	ratio := ebt / profit
-
-// 	fmt.Println(ebt)
-// 	fmt.Println(profit)
-// 	fmt.Println(ratio)
-// }
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main(){
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+    if err != nil {
+      fmt.Println(err)
+      // return
+      panic(err)
+    }
+	expenses, err := getUserInput("Expenses: ")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+	taxRate, err := getUserInput("Tax Rate: ")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
+  storeResult(ebt, profit, ratio)
 
 }
 
-func getUserInput(infoText string) float64 {
+func storeResult (ebt, profit, ratio float64){
+  results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+  os.WriteFile("results.txt", []byte(results), 0644 )
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value most be a positive number")
+	}
+
+
+	return userInput, nil
 }
 
 func calculateFinancials (revenue, expenses, taxRate float64) (float64, float64, float64) {
