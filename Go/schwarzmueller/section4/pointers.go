eb3af3b7296467e41263c7364f58eb52afcfecf9
Nandor <n3ndor@gmail.com>
2025-01-15 18:17:19 -0300

    understanding pointers

--- Go/schwarzmueller/section4/pointers.go
+++ Go/schwarzmueller/section4/pointers.go
@@ -5,15 +5,17 @@ import "fmt"
 func main() {
 	age:= 32 //regular variable
 
-	// agePointer := &age
-	var agePointer = &age
+	// var agePointer *int
+	// agePointer = &age
+	agePointer := &age
 
 	fmt.Println("Age: ", *agePointer)
 
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18
+func editAgeToAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
 }
