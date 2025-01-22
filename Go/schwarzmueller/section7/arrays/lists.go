1e73eb8dd2ce5d1d93431f577c609017ec34fb0b
Nandor <n3ndor@gmail.com>
2025-01-22 17:22:19 -0300

    slices

--- Go/schwarzmueller/section7/arrays/lists.go
+++ Go/schwarzmueller/section7/arrays/lists.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
+
+	fmt.Println(productNames)
+	
+	fmt.Println(prices[2])
+
+	featuredPrices := prices[1:3] //creating a slice. [start:end], [:] = all elements
+	highlightedPrices := featuredPrices[:1] //slice of slice
+	fmt.Println(highlightedPrices)
+	fmt.Println(len(prices), cap(prices)) // length 4, capacity 4
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices)) // 1 3 
 }
