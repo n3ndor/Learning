3ddc34b9eb2e6301dcb61b496455f84d2c779b3b
Nandor <n3ndor@gmail.com>
2025-02-01 15:31:50 -0300

    goroutines and channels

--- Go/schwarzmueller/section10/main.go
+++ Go/schwarzmueller/section10/main.go
@@ -5,18 +5,41 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	// dones := make([]chan bool, 4)
+	done := make(chan bool)
+
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you!", done)
+	// dones[1] = make(chan bool)
+	go greet("How are you?", done)
+	// dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", done)
+	// dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", done)
+
+	for range done {
+		// fmt.Println(doneChan)
+	}
+
+	// <- done
+	// <- done
+	// <- done
+	// <- done
+	// for _, done := range dones {
+	// 	<- done
+	// }
+
 }
