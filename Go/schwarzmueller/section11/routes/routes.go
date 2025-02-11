e5132b0f9fa2d64b17cf61772f6ecf52ec9da45e
Nandor <n3ndor@gmail.com>
2025-02-11 15:09:08 -0300

    middleware

--- Go/schwarzmueller/section11/routes/routes.go
+++ Go/schwarzmueller/section11/routes/routes.go
@@ -1,14 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/rest-api/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
  
 	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id") // /events/1, /events/5
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/;id", deleteEvent)
+
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
