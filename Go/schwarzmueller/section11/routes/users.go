59f965996a7e4525a7fea79a1a32487e17a70c14
Nandor <n3ndor@gmail.com>
2025-02-10 22:18:43 -0300

    login, hash

---
+++ Go/schwarzmueller/section11/routes/users.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message":"Could not parse request data."})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message":"User created successfully."})
+}
+
+func login(context *gin.Context){
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message":"Couldn't log in."})
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message":"Login successfull!"})
+}
