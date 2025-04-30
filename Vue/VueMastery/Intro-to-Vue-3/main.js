78c59f51a5914814b3aa5047b0d1803cc3d65a4d
Nandor <n3ndor@gmail.com>
2025-04-30 12:38:32 -0300

    8 computed properties

--- Vue/VueMastery/Intro-to-Vue-3/main.js
+++ Vue/VueMastery/Intro-to-Vue-3/main.js
@@ -3,33 +3,56 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       description: "A warm fuzzy pair of socks.",
-      image: "./assets/images/socks_green.jpg",
-      url: "https:/www.google.com",
-      //   inStock: false,
-      inventory: 8,
+      selectedVariant: 0,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
       sizes: ["S", "M", "L", "XL"],
     };
   },
-
   methods: {
     addToCart() {
       this.cart += 1;
     },
     removeFromCart() {
-      // this.cart -= 1;
       if (this.cart >= 1) {
         this.cart -= 1;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " " + "is on sale";
+      }
+      return "";
     },
   },
 });
