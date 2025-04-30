d3cea181f6dd66facc3a83bc9035fc0d0bf40aab
Nandor <n3ndor@gmail.com>
2025-04-30 11:48:46 -0300

    6 Event Handling

--- Vue/VueMastery/Intro-to-Vue-3/main.js
+++ Vue/VueMastery/Intro-to-Vue-3/main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       description: "A warm fuzzy pair of socks.",
       image: "./assets/images/socks_green.jpg",
@@ -10,10 +11,25 @@ const app = Vue.createApp({
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: ["S", "M", "L", "XL"],
     };
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      // this.cart -= 1;
+      if (this.cart >= 1) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
